6ed8d6c076e42465b878be788f86d9771c64ad46
Pedro Pacheco <pedropacheco@ptisp.pt>
2015-05-20 18:13:06 +0100

    load gravatar img

--- static/assets/js/views/header.js
+++ static/assets/js/views/header.js
@@ -1,10 +1,13 @@
 window.HeaderView = Backbone.View.extend({
   events: {
   },
-
+  getgravatar: function() {
+    var pic = sessionStorage.getItem('user');
+    $('.headgravatar',this.el).attr('src','http://www.gravatar.com/avatar/'+pic);
+  },
   render: function() {
     $(this.el).html(this.template());
-
+    this.getgravatar();
     return this;
   }
 
--- static/assets/js/views/login.js
+++ static/assets/js/views/login.js
@@ -1,7 +1,17 @@
 window.LoginView = Backbone.View.extend({
   events: {
+    'click .btnlogin': 'login'
   },
-
+  createhash: function(input) {
+    return CryptoJS.MD5(input).toString();
+  },
+  login: function() {
+    console.log(this.createhash($('.usernameinput').val()));
+    sessionStorage.setItem('user',this.createhash($('.usernameinput').val()));
+    app.navigate("/home", {
+      trigger: true
+    });
+  },
   render: function() {
     $(this.el).html(this.template());
 
