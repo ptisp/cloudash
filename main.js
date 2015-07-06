9d2e94989e372c2abd3b134d514869f2ae018d42
Pedro Dias <petermdias@gmail.com>
2015-07-06 19:18:58 +0100

    logs

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var express = require('express'),
   errorhandler = require('errorhandler'),
   api = require('./lib/routes/index'),
   auth = require('./lib/http/auth.js'),
+  logger = require('./lib/http/logger.js'),
   admin = require('./lib/http/admin.js');
 
 var app = express();
@@ -14,34 +15,34 @@ app.use(bodyParser());
 app.use(errorhandler());
 
 app.post('/api/user/login', auth, api.user.validateLogin);
-app.post('/api/user', auth, admin, api.user.createUser);
-app.put('/api/user', auth, api.user.updateUser);
+app.post('/api/user', auth, admin, logger, api.user.createUser);
+app.put('/api/user', auth, logger, api.user.updateUser);
 app.get('/api/listusers', auth, admin, api.user.listUsers);
 app.get('/api/user/:user', auth, api.user.getUser);
-app.delete('/api/remove/:user', auth, admin, api.user.removeUser);
+app.delete('/api/remove/:user', auth, admin, logger, api.user.removeUser);
 
-app.post('/api/vm/:id/start', auth, api.vm.startVm);
-app.post('/api/vm/:id/pause', auth, api.vm.pauseVm);
-app.post('/api/vm/:id/stop/:forced', auth, api.vm.stopVm);
-app.post('/api/vm/:id/stop', auth, api.vm.stopVm);
-app.post('/api/vm/:id/restart/:forced', auth, api.vm.restartVm);
-app.post('/api/vm/:id/restart', auth, api.vm.restartVm);
-app.post('/api/vm', auth, api.vm.createVm);
+app.post('/api/vm/:id/start', auth, logger, api.vm.startVm);
+app.post('/api/vm/:id/pause', auth, logger, api.vm.pauseVm);
+app.post('/api/vm/:id/stop/:forced', auth, logger, api.vm.stopVm);
+app.post('/api/vm/:id/stop', auth, logger, api.vm.stopVm);
+app.post('/api/vm/:id/restart/:forced', auth, logger, api.vm.restartVm);
+app.post('/api/vm/:id/restart', auth, logger, api.vm.restartVm);
+app.post('/api/vm', auth, logger, api.vm.createVm);
 app.get('/api/vm/:id/metrics', auth, api.vm.statsVm);
 
 app.get('/api/vm/:id/vnc', auth, api.vm.vmVNC);
-app.delete('/api/vm/:id', auth, api.vm.deleteVm);
+app.delete('/api/vm/:id', auth, logger, api.vm.deleteVm);
 app.get('/api/vm/:id', auth, api.vm.vmDetails);
-app.put('/api/vm/:id', auth, api.vm.vmResize);
+app.put('/api/vm/:id', auth, logger, api.vm.vmResize);
 app.get('/api/vm', auth, api.vm.vmList);
 
 app.get('/api/image', auth, api.vm.imageList);
 
 app.get('/api/support/:id', auth, api.support.ticketDetails);
-app.post('/api/support/:id', auth, api.support.replyticket);
-app.delete('/api/support/:id', auth, api.support.closeticket);
+app.post('/api/support/:id', auth, logger, api.support.replyticket);
+app.delete('/api/support/:id', auth, logger, api.support.closeticket);
 app.get('/api/support', auth, api.support.ticketList);
-app.post('/api/support', auth, api.support.openTicket);
+app.post('/api/support', auth, logger, api.support.openTicket);
 
 app.get('/api/config/logo', api.conf.getLogo);
 app.delete('/api/config/logo', auth, admin, api.conf.clearLogo);
