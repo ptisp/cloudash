944b8b2e13d774014a188bb79336f97835a27939
Pedro Dias <petermdias@gmail.com>
2015-07-13 17:30:39 +0100

    opennebula into vendors

--- vendors.js
+++ vendors.js
@@ -1,9 +1,13 @@
 require('colors');
 
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient,
+  OpenNebula = require('opennebula');
 
 MongoClient.connect(process.env.CLOUDY_MONGODB, function(err, db) {
   if (err) throw err;
   exports.mongo = db;
   console.log('(SYSTEM) Connected to MongoDB.'.green);
 });
+
+
+exports.one = new OpenNebula(process.env.ONE_CREDENTIALS_DEV, process.env.ONE_HOST_DEV);
