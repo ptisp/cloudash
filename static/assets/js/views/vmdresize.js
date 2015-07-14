5c6f14e9ff3fab270a6f10111ad925b29796e6bf
Pedro Pacheco <pedro.mpp@gmail.com>
2015-07-14 16:18:04 +0100

    add remove interface

--- static/assets/js/views/vmdresize.js
+++ static/assets/js/views/vmdresize.js
@@ -3,7 +3,34 @@ window.VMDResizeView = Backbone.View.extend({
     this.vm = options.vm;
   },
   events: {
-    'click #resizevm': 'vmresize'
+    'click #resizevm': 'vmresize',
+    'click #addinterface': 'addinterface',
+    'click .delinterface': 'delinterface'
+  },
+
+  delinterface: function(evt) {
+    var nicid = $(evt.target).attr('data-nicid');
+    modem('DELETE', 'vm/' + this.model.get('id')+'/network/'+nicid,
+      function(json) {
+        console.log(json);
+      },
+      function(xhr, ajaxOptions, thrownError) {
+        var json = JSON.parse(xhr.responseText);
+        showError('ERRO - Eliminação de Interface', json.error);
+      }, vmdetails
+    );
+  },
+
+  addinterface: function() {
+    modem('POST', 'vm/' + this.model.get('id')+'/network',
+      function(json) {
+        console.log(json);
+      },
+      function(xhr, ajaxOptions, thrownError) {
+        var json = JSON.parse(xhr.responseText);
+        showError('ERRO - Adicção de Interface', json.error);
+      }, vmdetails
+    );
   },
 
   vmresize: function(evt) {
@@ -38,7 +65,7 @@ window.VMDResizeView = Backbone.View.extend({
       html += '<div><span>IP: '+intf[i].ip+'</span></div>';
       html += '<div><span>MAC: '+intf[i].mac+'</span></div>';
       if (intf[i].id !== '0'){
-        html += '<button type="button" class="btn btn-lg btn-block btn-danger deletevm"> <i class="icon-delete"></i> <span data-i18n="">  Apagar</span> </button>';
+        html += '<button type="button" data-nicid="'+intf[i].id+'" class="btn btn-lg btn-block btn-danger delinterface"> <i class="icon-delete"></i> <span data-i18n="">  Apagar</span> </button>';
       }
       html += '</setion>';
       $('#interfaces', this.el).append(html);
