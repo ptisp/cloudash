318cb5427440ba15a9b49b1d370f1039d1bd473b
Pedro Dias <petermdias@gmail.com>
2015-07-14 18:37:56 +0100

    config refactoring

--- vendors.js
+++ vendors.js
@@ -12,9 +12,16 @@ MongoClient.connect(config.mongodb || process.env.CLOUDY_MONGODB, function(err, 
 });
 
 
-var onehost = config.onehost || process.env.ONE_HOST_DEV || process.env.ONE_HOST;
-var oneauth = config.oneauth || process.env.ONE_CREDENTIALS_DEV || process.env.ONE_CREDENTIALS;
+var onehost = process.env.ONE_HOST_DEV || process.env.ONE_HOST;
+var oneauth = process.env.ONE_CREDENTIALS_DEV || process.env.ONE_CREDENTIALS;
+
+if(config.providers.one) {
+  onehost = config.providers.one.host;
+  oneauth = config.providers.one.auth;
+}
 
 exports.one = new OpenNebula(oneauth, onehost);
 exports.onehost = onehost.replace(':2633/RPC2', '');
 exports.oneauth = oneauth;
+exports.onesunstone = config.providers.one.sunstone;
+exports.onenetwork = config.providers.one.network;
